9e671ea95e18d3d265c20fe545a98cf6bbd6c213
bender <kudin.dima@gmail.com>
2026-03-27 12:09:35 +0000

    Switch to version 97: modified src/app/live-testing/page.tsx

--- src/app/live-testing/page.tsx
+++ src/app/live-testing/page.tsx
@@ -37,11 +37,11 @@ export default function LiveTestingPage() {
         headingFontWeight="semibold"
       >
         {/* Picture over navigation bar */}
-        <div className="relative w-full bg-[#0a0f1e] py-2">
+        <div className="relative w-full bg-[#0a0f1e] py-2 flex justify-end px-4">
           <img
-            src="https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3BD3dlR7FZEi8PoUil2VldNiisn/uploaded-1774438142804-lhst1ilk.png"
+            src="https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3BD3dlR7FZEi8PoUil2VldNiisn/uploaded-1774610235649-b1wh8qy6.png"
             alt="VoiceLinkAI Logo"
-            className="absolute top-1/2 -translate-y-1/2 right-4 h-8"
+            className="h-12 w-auto object-contain"
           />
         </div>
         <div id="nav" data-section="nav">
